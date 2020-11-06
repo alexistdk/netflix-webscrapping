4e10439cc2c1140726f8f41aa592bb49dd4b5d80
Alexis Delgado <alexisndelgado@gmail.com>
2020-11-06 17:25:26 -0300

    agregado el test de la sinopsis

--- Testing.py
+++ Testing.py
@@ -27,3 +27,17 @@ class MyTestCase(TestCase):
     def test_id_dunkirk(self):
         id_pelicula = Scrapping.get_id("https://www.netflix.com/title/80170278")
         self.assertEqual("80170278", id_pelicula)
+
+    def test_categoria_pelicula(self):
+        categoria = Scrapping.get_category("https://www.netflix.com/browse/genre/2595")
+        self.assertEqual("Science & Nature Docs", categoria)
+
+    def test_categoria_serie(self):
+        categoria = Scrapping.get_category("https://www.netflix.com/browse/genre/27346")
+        self.assertEqual("Kids TV Shows", categoria)
+
+    def test_sinopsis_back_to_the_future(self):
+        sinopsis_scrappeada = Scrapping.get_sinopsis("https://www.netflix.com/title/60010110")
+        sinopsis = "After he accidentally drives a DeLorean time machine from 1985 to 1955, " \
+                   "Marty McFly races the clock to ensure his future parents fall in love."
+        self.assertEqual(sinopsis, sinopsis_scrappeada)
