d3a8cf82331e9d0ab061dcf576af7661168de092
Alexis Delgado <alexisndelgado@gmail.com>
2020-11-06 18:47:42 -0300

    agregada la función de que tiene que hacer con las películas

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from Pelicula import *
 
 
-def scrappear_pelicuas():
+def scrappear_peliculas():
     Pelicula.scrap_movies()
     Pelicula.csv_peliculas()
 
 
 if __name__ == "__main__":
-    scrappear_pelicuas()
+    scrappear_peliculas()
