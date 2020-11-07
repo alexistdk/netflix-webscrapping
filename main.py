3f00da543a47371f3a01028673a7fe5b9c0edbe9
Alexis Delgado <alexisndelgado@gmail.com>
2020-11-07 20:37:42 -0300

    main terminado

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from Pelicula import *
 from Webdriver import *
+from Serie import *
 
 
 def scrappear_peliculas():
-    Pelicula.scrap_movies()
+    Pelicula.scrapp_movies()
     Pelicula.csv_peliculas()
+
+
+def scrappear_series():
+    Serie.scrapp_series()
+    Serie.csv_series()
 
 
 if __name__ == "__main__":
     Webdriver.web_driver()
     scrappear_peliculas()
+    scrappear_series()
